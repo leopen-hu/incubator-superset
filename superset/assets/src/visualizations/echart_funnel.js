3f2f4c91dd8520943074558aae022e644a4ed8e4
leopen <790480953@qq.com>
2018-06-04 09:43:24 +0800

    modify the data for echarts demo

--- superset/assets/src/visualizations/echart_funnel.js
+++ superset/assets/src/visualizations/echart_funnel.js
@@ -10,75 +10,65 @@ function echartsFunnelVis(slice, payload) {
 
   let myChart = echarts.init(document.getElementById(sliceId));
   let option = {
-    tooltip: {
-        trigger: 'item',
-        formatter: "{a} <br/>{b} : {c}"
+    tooltip : {
+        trigger: 'axis'
     },
+    toolbox: {
+        show : true,
+        feature : {
+            mark : {show: true},
+            dataView : {show: true, readOnly: false},
+            magicType: {show: true, type: ['line', 'bar']},
+            restore : {show: true},
+            saveAsImage : {show: true}
+        }
+    },
+    calculable : true,
     legend: {
-        data: ['展现','点击','访问','咨询','订单']
+        data:['蒸发量','降水量','平均温度']
     },
-    calculable: true,
-    series : [{
-          name:'漏斗图',
-          type:'funnel',
-          x: '10%',
-          y: 60,
-          //x2: 80,
-          y2: 60,
-          width: '80%',
-          // height: {totalHeight} - y - y2,
-          min: 0,
-          max: 100,
-          minSize: '0%',
-          maxSize: '100%',
-          sort : 'descending', // 'ascending', 'descending'
-          gap : 10,
-          itemStyle: {
-              normal: {
-                  // color: 各异,
-                  borderColor: '#fff',
-                  borderWidth: 1,
-                  label: {
-                      show: true,
-                      position: 'inside'
-                      // textStyle: null      // 默认使用全局文本样式，详见TEXTSTYLE
-                  },
-                  labelLine: {
-                      show: false,
-                      length: 10,
-                      lineStyle: {
-                          // color: 各异,
-                          width: 1,
-                          type: 'solid'
-                      }
-                  }
-              },
-              emphasis: {
-                  // color: 各异,
-                  borderColor: 'red',
-                  borderWidth: 5,
-                  label: {
-                      show: true,
-                      formatter: '{b}:{c}%',
-                      textStyle:{
-                          fontSize:20
-                      }
-                  },
-                  labelLine: {
-                      show: true
-                  }
-              }
-          },
-          data:[
-              {value:60, name:'访问'},
-              {value:40, name:'咨询'},
-              {value:20, name:'订单'},
-              {value:80, name:'点击'},
-              {value:100, name:'展现'}
-          ]
-      }
+    xAxis : [
+        {
+            type : 'category',
+            data : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月']
+        }
+    ],
+    yAxis : [
+        {
+            type : 'value',
+            name : '水量',
+            axisLabel : {
+                formatter: '{value} ml'
+            }
+        },
+        {
+            type : 'value',
+            name : '温度',
+            axisLabel : {
+                formatter: '{value} °C'
+            }
+        }
+    ],
+    series : [
+
+        {
+            name:'蒸发量',
+            type:'bar',
+            data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+        },
+        {
+            name:'降水量',
+            type:'bar',
+            data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+        },
+        {
+            name:'平均温度',
+            type:'line',
+            yAxisIndex: 1,
+            data:[2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+        }
     ]
-  };
+};
 
   const fd = slice.formData;
   const json = payload.data;
